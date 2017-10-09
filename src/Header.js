2a2ff80d22a91957b8b4d8c9981b77d97f3a88c5
Tim Kellogg <tim.kellogg@gmail.com>
2017-10-09 12:09:16 -0700

    fix animations to make more smooth

--- src/Header.js
+++ src/Header.js
@@ -7,12 +7,12 @@ export default () => {
     <header className="Header">
       <h1 className="Header-title">Tim</h1>
       <h1 className="Header-title">Kellogg</h1>
-      <h2>Software Engineer</h2>
-      <p className="Header-subtitle">
+      <h2 className="Header-subtitle">Software Engineer</h2>
+      <div>
         <span className="Header-spacer">Client </span>
         <span className="Header-spacer">Server </span>
         <span className="Header-spacer">Cloud </span>
-      </p>
+      </div>
     </header>
   )
 }
