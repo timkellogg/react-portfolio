0c68eb04d160ec6081e8ffc283214e1e9cdb7c24
Tim Kellogg <tim.kellogg@gmail.com>
2017-10-09 11:30:44 -0700

    add basic project list animations

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import './ProjectList.css';
 import fitnessImg from './img/fitness.jpg';
 import whiskeyImg from './img/whiskey.jpg';
@@ -9,31 +9,33 @@ import povertyImg from './img/poverty.jpg';
 import burgerImg from './img/burger.jpg';
 import shaveImg from './img/shave.jpg';
 
-export default () => {
-  return (
-    <section className="ProjectList">
-      <div className="ProjectList-Content">
-        <div className="ProjectList-List">
-          <div className="ProjectList-Item">
-             <img className="ProjectList-Img" src={fitnessImg} alt="stationary cycles"/>
-          </div>
-          <div className="ProjectList-Item">
-             <img className="ProjectList-Img" src={whiskeyImg} alt="a glass of whiskey"/>
-          </div>
-          <div className="ProjectList-Item">
-             <img className="ProjectList-Img" src={tanfImg} alt="city skyline"/>
-          </div>
-          <div className="ProjectList-Item">
-             <img className="ProjectList-Img" src={povertyImg} alt="children laughing"/>
-          </div>
-          <div className="ProjectList-Item">
-             <img className="ProjectList-Img" src={shaveImg} alt="man shaving"/>
-          </div>
-          <div className="ProjectList-Item">
-             <img className="ProjectList-Img" src={burgerImg} alt="hamburger"/>
+export default class ProjectList extends Component {
+  render() {
+    return (
+      <section className="ProjectList">
+        <div className="ProjectList-Content">
+          <div className="ProjectList-List">
+            <div className="ProjectList-Item">
+               <img className="ProjectList-Img" src={fitnessImg} alt="stationary cycles"/>
+            </div>
+            <div className="ProjectList-Item">
+               <img className="ProjectList-Img" src={whiskeyImg} alt="a glass of whiskey"/>
+            </div>
+            <div className="ProjectList-Item">
+               <img className="ProjectList-Img" src={tanfImg} alt="city skyline"/>
+            </div>
+            <div className="ProjectList-Item">
+               <img className="ProjectList-Img" src={povertyImg} alt="children laughing"/>
+            </div>
+            <div className="ProjectList-Item">
+               <img className="ProjectList-Img" src={shaveImg} alt="man shaving"/>
+            </div>
+            <div className="ProjectList-Item">
+               <img className="ProjectList-Img" src={burgerImg} alt="hamburger"/>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
