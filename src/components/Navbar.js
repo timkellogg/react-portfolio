1c110597e9242370fe9b40b6f0ad5a9d761f1ac0
Tim Kellogg <tim.kellogg@gmail.com>
2017-10-09 23:32:51 -0700

    refactor navbar into functional component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,16 +4,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-export default class Navbar extends Component {
-  render() {
-    const onLanding = window.location.pathname === '/';
-
-    return (
-      <nav className="Navbar">
-        {onLanding ? '' : <Link className="Navbar-link" to="/">Home</Link>}
-        <Link className="Navbar-link" to="/about">About</Link>
-        <Link className="Navbar-link" to="/contact">Contact</Link>
-      </nav>
-    );
-  }
+export default () => {
+  const onLanding = window.location.pathname === '/';
+  return (
+    <nav className="Navbar">
+      {onLanding ? '' : <Link className="Navbar-link" to="/">Home</Link>}
+      <Link className="Navbar-link" to="/about">About</Link>
+      <Link className="Navbar-link" to="/contact">Contact</Link>
+    </nav>
+  );
 }
