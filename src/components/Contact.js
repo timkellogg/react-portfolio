d22fb5a96ad1a8fff8bd8b8c42c2770407b2670d
Tim Kellogg <tim.kellogg@gmail.com>
2017-10-15 09:47:22 -0700

    add basic contact page WIP

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,29 +1,59 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './Contact.css';
 import Navbar from './Navbar';
 
-export default () => {
-  return (
-    <main className="Contact grid">
-      <section className="panel--left">
-        <Navbar />
-        <h1>Need a website?</h1>
-      </section>
-      <section className="panel--right">
-        <form action="https://somepage/messages" method="post" className="Contact-form panel-content">
-          <label for="name">Name</label><br />
-          <input name="name" required type="text" /><br />
+export default class Contact extends Component {
+  constructor(props) {
+    super(props);
 
-          <label for="email">Email:</label><br />
-          <input name="email" required type="email" /><br />
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+    }
+  }
 
-          <label for="message">Message</label><br />
-          <textarea name="message" required></textarea><br />
-          <button type="submit">Send</button>
-        </form>
-      </section>
-    </main>
-  );
+  onSubmit(event) {
+    const endpoint = 'http://localhost:3000/contacts';
+    const { name, email, message } = this.state;
+
+    event.preventDefault();
+
+    axios.post(endpoint, { name, email, message }).catch(res => {debugger});
+  }
+
+  onChange(event) {
+    const field = event.target.name;
+    const value = event.target.value;
+
+    this.setState({[field]: value});
+  }
+
+  render() {
+    return (
+      <main className="Contact grid">
+        <section className="panel--left">
+          <Navbar />
+          <h1>Need a website?</h1>
+        </section>
+        <section className="panel--right">
+          <form className="Contact-form panel-content" onSubmit={this.onSubmit.bind(this)}>
+            <label for="name">Name</label><br />
+            <input name="name" value={this.state.name.value} onChange={this.onChange.bind(this)} type="text" /><br />
+
+            <label for="email">Email:</label><br />
+            <input name="email" value={this.state.email.value} onChange={this.onChange.bind(this)} type="email" /><br />
+
+            <label for="message">Message</label><br />
+            <textarea name="message" value={this.state.message.value} onChange={this.onChange.bind(this)}></textarea><br />
+            <button>Send</button>
+          </form>
+        </section>
+      </main>
+    );
+  }
 }
+// action="http://localhost:4000/contacts" method="POST" 
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,40 +1,56 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import './ProjectList.css';
 
 import fitnessImg from '../assets/img/fitness.jpg';
 import whiskeyImg from '../assets/img/whiskey.jpg';
 import tanfImg    from '../assets/img/tanf.jpg';
 import povertyImg from '../assets/img/poverty.jpg';
-import burgerImg  from '../assets/img/burger.jpg';
 import shaveImg   from '../assets/img/shave.jpg';
+import burgerImg  from '../assets/img/burger.jpg';
 
-export default () => {
-  return (
-    <section className="ProjectList">
-      <div className="panel-content">
-        <div className="ProjectList-List">
-          <div className="ProjectList-Item">
-              <img className="ProjectList-Img" src={fitnessImg} alt="stationary cycles"/>
-          </div>
-          <div className="ProjectList-Item">
-              <img className="ProjectList-Img" src={whiskeyImg} alt="a glass of whiskey"/>
-          </div>
-          <div className="ProjectList-Item">
-              <img className="ProjectList-Img" src={tanfImg} alt="city skyline"/>
-          </div>
-          <div className="ProjectList-Item">
-              <img className="ProjectList-Img" src={povertyImg} alt="children laughing"/>
-          </div>
-          <div className="ProjectList-Item">
-              <img className="ProjectList-Img" src={shaveImg} alt="man shaving"/>
-          </div>
-          <div className="ProjectList-Item">
-              <img className="ProjectList-Img" src={burgerImg} alt="hamburger"/>
+const images = [
+  { image: fitnessImg, alt: 'woman exercising' },
+  { image: whiskeyImg, alt: 'bar' },
+  { image: tanfImg, alt: 'office building' },
+  { image: povertyImg, alt: 'poor children' },
+  { image: burgerImg, alt: 'burger' },
+  { image: shaveImg, alt: 'old salon' },
+];
+
+export default class ProjectList extends Component {
+
+  onHover() {
+    debugger
+  }
+
+  renderImages() {
+    const component = this;
+
+    return images.map((data, index) => {
+      return (
+        <div key={index} className="ProjectList-Item">
+          <img
+            onMouseEnter={component.onHover.bind(component)}
+            className="ProjectList-Img"
+            src={data.image}
+            alt={data.alt}
+          />
+        </div>
+      )
+    });
+  }
+
+  render() {
+    return (
+      <section className="ProjectList">
+        <div className="panel-content" >
+          <div className="ProjectList-List">
+            {this.renderImages()}
           </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
